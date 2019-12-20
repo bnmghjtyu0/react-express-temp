85c930692cd8a91c0059fca21085c7319ec289c4
RichardLiao <bnmghjtyu0@gmail.com>
2019-12-20 15:46:16 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,54 +2,96 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import server from './server/index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [form, setForm] = React.useState({ email: '', password: '' })
+  const [formRegister, setFormRegister] = React.useState({ name: '', email: '', password: '' })
   const [data, setData] = React.useState({});
 
-  const getTitle = async () => {
-    const res = await axios.get("/backend/demo", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('token')
-      }
-    })
-    setData(res.data)
-  }
   React.useEffect(() => {
-    getTitle()
+    _apiDemo()
   }, []);
 
-  const sendForm = (e) => {
+  const _apiSignUp = async () => {
+    const res = await server._apiAuth('users/register', formRegister)
+    console.log('sign', res)
+  }
+
+  const _apiLogin = async () => {
+    const res = await server._apiAuth('users/login', {
+      'email': 'richard.liao@program.com.tw',
+      'password': '1qaz@WSX'
+    })
+    console.log('login', res)
+    if (res.data.retCode === 1) {
+      localStorage.setItem('token', res.data.retData.token)
+    } else {
+      localStorage.setItem('token', '')
+    }
+  }
+  const _apiDemo = async () => {
+    const res = await server._apiFull('demo')
+    setData(res.data)
+  }
+
+  const sendFormSignUp = async (e) => {
     e.preventDefault();
-    axios.post("/backend/users/login", {
-      'email': form.email,
-      'password': form.password
-    }).then(res => {
-      if (res.data.retCode === 1) {
-        localStorage.setItem('token', res.data.retData.token)
-      } else {
-        localStorage.setItem('token', '')
-      }
-    });
+    _apiSignUp()
+  }
+
+  const sendFormSignIn = async (e) => {
+    e.preventDefault();
+    _apiLogin()
   }
 
 
   return (
     <div className="App">
-      <form onSubmit={sendForm}>
-        <input placeholder="請輸入帳號" value={form.email} onChange={(e) => {
-          setForm({ ...form, email: e.target.value })
-        }} />
-        <input placeholder="請輸入密碼" value={form.password} onChange={(e) => {
-          setForm({ ...form, password: e.target.value })
-        }} />
-        <button type="submit">登入</button>
-      </form>
+
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{data.title}</p>
+        {!!localStorage.getItem('token')
+          && <div>
+            <p>{data.title}</p>
+            <div>
+              <button onClick={() => localStorage.removeItem('token')}>logout</button>
+            </div>
+          </div>
+        }
+        {!localStorage.getItem('token') &&
+          <div>
+            <form onSubmit={sendFormSignUp}>
+              <div className="form-group mb-2">
+                <div className="mr-2">
+                  <input className="form-control mr-2 mb-2" placeholder="請輸入名稱" value={formRegister.name} onChange={(e) => {
+                    setFormRegister({ ...formRegister, name: e.target.value })
+                  }} />
+                  <input className="form-control mr-2 mb-2" placeholder="請輸入帳號" value={formRegister.email} onChange={(e) => {
+                    setFormRegister({ ...formRegister, email: e.target.value })
+                  }} />
+                  <input className="form-control" placeholder="請輸入密碼" value={formRegister.password} onChange={(e) => {
+                    setFormRegister({ ...formRegister, password: e.target.value })
+                  }} />
+                </div>
+                <button type="submit" className="btn btn-sm btn-info">註冊</button>
+              </div>
+            </form>
+            <form onSubmit={sendFormSignIn}>
+              <div className="form-group">
+                <input className="form-control mr-2 mb-2" placeholder="請輸入帳號" value={form.email} onChange={(e) => {
+                  setForm({ ...form, email: e.target.value })
+                }} />
+                <input className="form-control" placeholder="請輸入密碼" value={form.password} onChange={(e) => {
+                  setForm({ ...form, password: e.target.value })
+                }} />
+              </div>
+              <button type="submit" className="btn btn-sm btn-success">登入</button>
+            </form>
+          </div>
+        }
       </header>
     </div>
   );
